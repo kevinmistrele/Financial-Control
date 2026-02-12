9f9878be17e2d3b08b2b568715ccad723d35216c
kevinmistrele <kevintavaresmistrele@gmail.com>
2026-02-11 22:35:05 -0300

    refactor: update transaction handling in CardsRecentTransactions component

--- frontend/src/components/dashboard/Transactions/CardsRecentTransactions.tsx
+++ frontend/src/components/dashboard/Transactions/CardsRecentTransactions.tsx
@@ -1,12 +1,12 @@
 import {Card, CardContent, CardHeader} from "@/components/ui/card";
-import {type GetExpenseDTO} from "@/types/expense";
+import { type TransactionItemType} from "@/types/expense";
 import {Button} from "@/components/ui/button";
 import {ListFilter} from "lucide-react";
 import {useState} from "react";
 import {TransactionsModal} from "@/components/dashboard/Transactions/modal/Transactions-Modal";
 import {ItemsCard} from "@/components/dashboard/Transactions/ItemsCard";
 
-const transactions: GetExpenseDTO[] = [
+let transactionsList: TransactionItemType[] = [
     { id: 1, description: "Grocery Shopping", amount: 75.50, date: "2024-06-10", category: "shopping" },
     { id: 2, description: "Electricity Bill", amount: 120.00, date: "2024-06-09", category: "bills" },
     { id: 3, description: "Restaurant", amount: 45.25, date: "2024-06-08", category: "food" },
@@ -16,12 +16,15 @@ const transactions: GetExpenseDTO[] = [
 
 
 export const CardsRecentTransactions = () => {
+
     const [openModal, setOpenModal] = useState(false);
+    const [transactions, setTransactions] = useState<TransactionItemType[]> (transactionsList);
 
     const toggleModal = () => {
         setOpenModal(!openModal);
         console.log('openModal:', openModal);
     }
+
 
     return (
         <Card>
@@ -35,15 +38,11 @@ export const CardsRecentTransactions = () => {
                 </div>
             </CardHeader>
             <CardContent className="pb-2 gap-3 mb-3 flex flex-col">
-                {transactions.map((transaction)=>
+                {transactions.map((transaction) =>
                     <ItemsCard
-                        description={transaction.description}
-                        id={transaction.id} date={transaction.date}
-                        amount={transaction.amount}
-                        iconCategory={transaction.category}
+                        transaction={transaction}
                     />
-
-                )}
+                    )}
             </CardContent>
         </Card>
 
