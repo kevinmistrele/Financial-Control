164e8d8aa2269fbf1351ac7242f06bc5b5eef57f
kevinmistrele <kevintavaresmistrele@gmail.com>
2026-02-13 14:04:59 -0300

    refactor: enhance CardsTracker component with props for dynamic content

--- frontend/src/components/dashboard/Cards/CardsTracker.tsx
+++ frontend/src/components/dashboard/Cards/CardsTracker.tsx
@@ -1,23 +1,31 @@
 import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
-import {Calendar} from "lucide-react";
+import {IconComponent} from "@/components/dashboard/Transactions/IconComponent";
+import type {LucideIcon} from "lucide-react";
 
-export const CardsTracker = () => {
+interface CardsTrackerProps {
+    cardTitle: string;
+    cardAmount: string;
+    cardIcon: LucideIcon;
+    cardDescription: string;
+}
+
+export const CardsTracker = ({cardTitle, cardAmount, cardIcon, cardDescription}: CardsTrackerProps) => {
     return (
             <Card className="w-full bg-[#0e0e11] border border-[#2c2c30] hover:border-[#14b8a5]">
                 <CardHeader className="pb-2">
-                    <CardTitle className="text-[#a1a1aa] text-sm">Total Spent (7 days)</CardTitle>
+                    <CardTitle className="text-[#a1a1aa] text-sm">{cardTitle}</CardTitle>
                 </CardHeader>
                 <CardContent className="pb-2">
                     <div className="flex flex-row justify-between items-center">
-                        <p className="text-2xl font-bold">$495.99</p>
-                        <div className="flex bg-[#14b8a51a] w-12 h-12 justify-center items-center rounded-lg">
-                            <Calendar className="text-[#14b8a5] size-8" />
+                        <p className="text-2xl font-bold">{cardAmount}</p>
+                        <div className="flex bg-[#14b8a51a] justify-center items-center rounded-lg">
+                           <IconComponent isCategory={false} iconName={cardIcon}/>
                         </div>
 
                     </div>
                 </CardContent>
                 <CardFooter>
-                    <p className="text-sm text-[#a1a1aa]">Last Week</p>
+                    <p className="text-sm text-[#a1a1aa]">{cardDescription}</p>
                 </CardFooter>
             </Card>
     )
