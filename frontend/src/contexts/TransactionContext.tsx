06b2e70be309dc9db010b008aa49fb4c69c07d79
kevinmistrele <kevintavaresmistrele@gmail.com>
2026-02-14 08:32:31 -0300

    refactor: add onSave function to TransactionContext for updating transactions

--- frontend/src/contexts/TransactionContext.tsx
+++ frontend/src/contexts/TransactionContext.tsx
@@ -7,6 +7,7 @@ import * as React from "react";
 interface TransactionContextType {
     transactions: TransactionItemType[];
     onDelete: (id: number) => void;
+    onSave: (data: {id: number, description: string, amount: number, category: string}) => void;
 }
 
 export const TransactionContext = React.createContext<TransactionContextType | undefined>(undefined);
@@ -18,9 +19,23 @@ export const TransactionProvider = ({ children }: {children: ReactNode}) => {
         const updatedTransactions = transactions.filter((transaction) => transaction.id !== id);
         setTransactions(updatedTransactions);
     }
+    const onSave = (data: any) => {
+        const updatedTransactions = transactions.map((transaction) => {
+            if(transaction.id === data.id) {
+                return {
+                    ...transaction,
+                    description: data.description,
+                    amount: data.amount,
+                    category: data.category
+                }
+            }
+            return transaction;
+        })
+        setTransactions(updatedTransactions);
+    }
 
     return (
-        <TransactionContext.Provider value={{transactions, onDelete}}>
+        <TransactionContext.Provider value={{transactions, onDelete, onSave}}>
             {children}
         </TransactionContext.Provider>
     )
