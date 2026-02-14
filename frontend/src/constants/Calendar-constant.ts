ac2c71c24d5cf4c69ceb1dbc7a10aaa4c51a404b
kevinmistrele <kevintavaresmistrele@gmail.com>
2026-02-14 18:11:58 -0300

    refactor: update month values in Calendar-constant to use zero-padded format

--- frontend/src/constants/Calendar-constant.ts
+++ frontend/src/constants/Calendar-constant.ts
@@ -1,16 +1,16 @@
 import {type MonthsType} from "@/types/expense";
 
 export const Months: MonthsType[] = [
-    { id: 1, value: "january", label: "January" },
-    { id: 2, value: "february", label: "February" },
-    { id: 3, value: "march", label: "March" },
-    { id: 4, value: "april", label: "April" },
-    { id: 5, value: "may", label: "May" },
-    { id: 6, value: "june", label: "June" },
-    { id: 7, value: "july", label: "July" },
-    { id: 8, value: "august", label: "August" },
-    { id: 9, value: "september", label: "September" },
-    { id: 10, value: "october", label: "October" },
-    { id: 11, value: "november", label: "November" },
-    { id: 12, value: "december", label: "December" },
+    { id: 1, value: "01", label: "January" },
+    { id: 2, value: "02", label: "February" },
+    { id: 3, value: "03", label: "March" },
+    { id: 4, value: "04", label: "April" },
+    { id: 5, value: "05", label: "May" },
+    { id: 6, value: "06", label: "June" },
+    { id: 7, value: "07", label: "July" },
+    { id: 8, value: "08", label: "August" },
+    { id: 9, value: "09", label: "September" },
+    { id: 10, value: "10", label: "October" },
+    { id: 11, value: "11", label: "November" },
+    { id: 12, value: "12", label: "December" },
 ]
